93190c9e012899551c8d16663d46c7f2fd46b2fe
javapractice88 <javapractice88@gmail.com>
2023-06-20 15:36:05 +0200

    Code for multiplicatin Table

--- src/main/java/com/javapractice/loops/dowhileloop/MultiplicationTable.java
+++ src/main/java/com/javapractice/loops/dowhileloop/MultiplicationTable.java
@@ -3,6 +3,17 @@ package com.javapractice.loops.dowhileloop;
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-
+        System.out.println("Multiplication Table:");
+        int i=1;
+        do {
+            int j =1;
+            do {
+                int mul = i*j; //1*1, 1*2, 1*3
+                System.out.print(mul + "\t");
+                j++;
+            } while (j<=10);
+            i++;
+            System.out.print("\n");
+        } while (i<=13);
     }
 }
