0bceaaf858fe4a9cb4e7610af5f3b5778df39dc3
salma1313 <sk.salma13@gmail.com>
2023-06-21 15:01:21 +0530

    multiplication table

---
+++ src/main/java/com/javapractice/loops/forloop/nestedforloops/forLoopTable.java
@@ -0,0 +1,22 @@
+package com.javapractice.loops.forloop.nestedforloops;
+
+public class forLoopTable {
+    public static void main(String[] args) {
+
+
+        for (int s = 1;  s<=10; s++){
+
+
+            for(int a= 1; a<=10;a++){
+
+            int l = s * a;
+            System.out.println(s+ "X" +a+ "=" +l);
+
+        }
+
+
+
+
+    }
+}
+}
