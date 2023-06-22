c3cddf5e92b796dadec17834cc0e73201a09d348
salma1313 <sk.salma13@gmail.com>
2023-06-22 14:15:04 +0530

    multiplication table change

--- src/main/java/com/javapractice/loops/forloop/nestedforloops/forLoopTable.java
+++ src/main/java/com/javapractice/loops/forloop/nestedforloops/forLoopTable.java
@@ -4,10 +4,10 @@ public class forLoopTable {
     public static void main(String[] args) {
 
 
-        for (int s = 1;  s<=10; s++){
+        for (int s = 1;  s<=15; s++){
 
 
-            for(int a= 1; a<=10;a++){
+            for(int a= 1; a<=15;a++){
 
             int l = s * a;
             System.out.println(s+ "X" +a+ "=" +l);
