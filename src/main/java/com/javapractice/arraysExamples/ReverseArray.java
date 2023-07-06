072c49efa9b70af8a4d706a780fd94944778c5c0
salma1313 <sk.salma13@gmail.com>
2023-07-06 21:17:29 +0530

    array

---
+++ src/main/java/com/javapractice/arraysExamples/ReverseArray.java
@@ -0,0 +1,18 @@
+package com.javapractice.arraysExamples;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        int[] a = {12, 32,45,11,22,98,80};
+
+
+
+        for (int i = a.length-1; i>=0;i--){
+
+            System.out.print(a[i] + ",");
+        }
+    }
+
+
+}
