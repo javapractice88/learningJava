8bdca463de734357fa91e66c95d2475051e3782d
salma1313 <sk.salma13@gmail.com>
2023-07-06 21:35:15 +0530

    array.

--- src/main/java/com/javapractice/arraysExamples/ReverseArray.java
+++ src/main/java/com/javapractice/arraysExamples/ReverseArray.java
@@ -6,11 +6,23 @@ public class ReverseArray {
 
         int[] a = {12, 32,45,11,22,98,80};
 
+        int[] b = new int [a.length] ;
 
+int j =0;
 
         for (int i = a.length-1; i>=0;i--){
 
-            System.out.print(a[i] + ",");
+
+
+            b[j] = a[i];
+
+            j++;
+
+        }
+
+        for ( int k= 0; k<= b.length-1; k++){
+
+            System.out.print(b[k] + ",");
         }
     }
 
