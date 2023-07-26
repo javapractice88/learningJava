9559411eac666818cb85af6953a9ee39cbcd85cd
javapractice88 <javapractice88@gmail.com>
2023-07-26 22:03:32 +0530

    sinx

---
+++ src/main/java/com/javapractice/loops/forloop/SinX.java
@@ -0,0 +1,32 @@
+package com.javapractice.loops.forloop;
+
+/**
+ * use scanner x=5
+ * n=10
+ *
+ * sum=0;
+ * 5+(5*5*5)/(1*2*3) + (5*5*5*5*5)/(1*2*3*4*5) + 57/7! + 59/9!
+ *
+ *
+ * for(int i=1;i<=n;i++) {
+ *   int power = 1;
+ *   int fact = 1;
+ *   if(i%10 != 0) {
+ * 	for(int j=1; j<=i;j++) {
+ * 	   power = power*x; 1*5=5; 5*5=25; 25*5=125
+ * 	   fact = fact*j; 1*1=1;1*2=2;2*3=6
+ *        }
+ * 	if() {
+ *
+ *    } else {
+ * 		sum = sum + (power/fact);
+ *    }
+ *   }
+ *
+ * }
+ */
+public class SinX {
+    public static void main(String[] args) {
+
+    }
+}
