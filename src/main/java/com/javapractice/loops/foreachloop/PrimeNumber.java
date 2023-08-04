39ae3d48cc05789612af9c3ec5c1008bfc8a54af
javapractice88 <javapractice88@gmail.com>
2023-08-04 22:33:17 +0530

    mirror inverse

--- src/main/java/com/javapractice/loops/foreachloop/PrimeNumber.java
+++ src/main/java/com/javapractice/loops/foreachloop/PrimeNumber.java
@@ -11,31 +11,18 @@ public class PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(" Enter a num: ");
-
         int primeNum = sc.nextInt();
-
         int p = 0;// temp variable
-
         for (int i = 2; i <= primeNum - 1; i++) {// 7
-
             if (primeNum % i == 0) {// 7%2, 7%3, 7%4,7%5,7%6
-
                 p = 1;
-
                 break;
-
             }
-
-
         }
         if (p == 0) {
-
             System.out.println("It's a prime num: " + primeNum);
         } else {
-
             System.out.println("It's not a prime num: " + primeNum);
         }
-
-
     }
 }
---
+++ src/main/java/com/javapractice/loops/forloop/MirrorInverse.java
@@ -0,0 +1,27 @@
+package com.javapractice.loops.forloop;
+
+public class MirrorInverse {
+
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 0 };
+
+        if (checkMirrorInverse(arr)) {
+            System.out.println("The array is mirror inverse");
+        } else {
+            System.out.println("The array is not a mirror inverse");
+        }
+
+    }
+
+    private static boolean checkMirrorInverse(int[] arr) {
+        for(int i = 0; i< arr.length; i++) {
+            if(arr[arr[i]] != i) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
